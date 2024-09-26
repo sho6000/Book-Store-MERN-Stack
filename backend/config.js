a78f719663c42e38b21789acf8fc347df8baa5f0
sho6000 <shounsalaji@gmail.com>
2024-09-26 22:07:07 +0530

    my lab10

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+  'mongodb://localhost:27017/';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
